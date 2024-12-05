ca6b330ba86a143e6b7e7613988254d4550ce652
mzsmzsmzs <zeeinsomniac@gmail.com>
2024-12-05 12:27:08 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -5,11 +5,24 @@ public class Fibonacci {
         int a = 0, b = 1, c;
         System.out.println("First " + n + " terms: ");
         for (int i = 1; i <= n; ++i) {
-            System.out.println(a+b);
+            System.out.println(a);  // Corrected to print `a`
             c = a + b;
             a = b;
             b = c;
         }
     }
 
+    public int[] generateFibonacci(int n) {
+        if (n <= 0) throw new IllegalArgumentException("n must be greater than 0");
+
+        int[] series = new int[n];
+        int a = 0, b = 1, c;
+        for (int i = 0; i < n; ++i) {
+            series[i] = a;
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return series;
+    }
 }
